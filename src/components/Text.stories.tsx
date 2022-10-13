6cc7e3560337759513779b0c70a3cc9ca9c67c72
Ciro Nogueira <cironogueira@edu.unirio.br>
2022-10-13 20:27:01 -0300

    Initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,73 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem Impsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    },
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm',
+  },
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      }
+    },
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg',
+  },
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      }
+    },
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Text with P</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    }
+  }
+}
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,47 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { InputHTMLAttributes, ReactNode } from 'react';
+
+export interface TextInputRootProps {
+	children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+	return (
+		<div className="px-3 flex items-center gap-3 bg-gray-800 rounded  w-full h-12 focus-within:ring-1 ring-cyan-300 transition-all">
+			{props.children}
+		</div>
+	);
+}
+TextInputRoot.displayName = 'TextInput.Root';
+
+export interface TextInputIconProps {
+	children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+	return(
+		<Slot className="w-6 h-6 text-gray-400">
+			{props.children}
+		</Slot>
+	);
+}
+TextInputIcon.displayName = 'TextInput.Icon';
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+function TextInputInput(props: TextInputInputProps) {
+	return (
+		<input
+			className="py-4 bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+			{...props}
+		/>
+	);
+}
+TextInputInput.displayName = 'TextInput.Input';
+
+export const TextInput = {
+	Root: TextInputRoot,
+	Input: TextInputInput,
+	Icon: TextInputIcon,
+}
